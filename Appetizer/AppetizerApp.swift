a58116708150b0d2d1dfccdfe436074057ad2128
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-30 20:41:30 +0300

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Николай Никитин on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
