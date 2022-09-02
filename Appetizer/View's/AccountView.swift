b0288d9422db36444a553089e8789b4b2bbced6a
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-09-02 22:30:23 +0300

    Basic UI for AccountView added

--- Appetizer/View's/AccountView.swift
+++ Appetizer/View's/AccountView.swift
@@ -8,16 +8,48 @@
 import SwiftUI
 
 struct AccountView: View {
-    var body: some View {
-      NavigationView {
-        Text("Account View")
-          .navigationTitle("Account")
+
+  //MARK: - View Poperties
+  @State private var firstName = ""
+  @State private var lastName = ""
+  @State private var eMail = ""
+  @State private var birthdate = Date()
+  @State private var extraNapkins = false
+  @State private var frequentRefills = false
+
+  //MARK: - View Body
+  var body: some View {
+    NavigationView {
+      Form {
+        Section(header: Text("Personal Info")) {
+          TextField("First Name...", text: $firstName)
+          TextField("Last Name...", text: $lastName)
+          TextField("E-mail...", text: $eMail)
+            .keyboardType(.emailAddress)
+            .textInputAutocapitalization(.none)
+            .disableAutocorrection(true)
+          DatePicker("Birthdate",
+                     selection: $birthdate,
+                     displayedComponents: .date)
+          Button {
+
+          } label: {
+            Text("Save")
+          }
+        }
+        Section(header: Text("Requests")) {
+          Toggle("Extra Napkins", isOn: $extraNapkins)
+          Toggle("Frequent Refills", isOn: $frequentRefills)
+        }
+        .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
       }
+      .navigationTitle("Account")
     }
+  }
 }
 
 struct AccountView_Previews: PreviewProvider {
-    static var previews: some View {
-        AccountView()
-    }
+  static var previews: some View {
+    AccountView()
+  }
 }
